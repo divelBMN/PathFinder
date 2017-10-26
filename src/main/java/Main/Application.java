1221070790d7d6126d4a3e107d88fc7940adc6da
Maxim Bezdenezhnykh <esgiliot@gmail.com>
2017-10-26 23:08:50 +0500

    adding classes Main and Pathfinder

---
+++ src/main/java/Main/Application.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		Pathfinder pathfinder = new Pathfinder();
+		Pathfinder.start();
+	}
+
+}
---
+++ src/main/java/Main/Pathfinder.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class Pathfinder {
+
+	public static void start() {
+	
+	}
+
+}
